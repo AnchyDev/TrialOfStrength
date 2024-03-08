6d431a726924f5e5d7b7fa9c0ebdab93a1fc03dd
AnchyDev <lewienato@gmail.com>
2024-03-08 17:55:42 +1100

    Added spell scaling.

--- src/scripts/ToSUnitScript.cpp
+++ src/scripts/ToSUnitScript.cpp
@@ -58,8 +58,53 @@ void ToSUnitScript::ModifyMeleeDamage(Unit* /*target*/, Unit* attacker, uint32& 
 
     uint32 currentWave = iScript->GetData(TOS_DATA_ENCOUNTER_CURRENT_WAVE);
 
-    uint32 baseDamage = 500;
-    uint32 damageDivider = 15;
+    uint32 baseDamage = sConfigMgr->GetOption<uint32>("TrialOfStrength.Scaling.BaseDamage.Physical", 500);
+    uint32 damageDivider = sConfigMgr->GetOption<uint32>("TrialOfStrength.Scaling.BaseDamage.PhysicalDivider", 15);;
+
+    uint32 newDamage = baseDamage * (1.0f + (float(currentWave) / float(damageDivider)));
+
+    damage = urand(newDamage / 2, newDamage);
+}
+
+void ToSUnitScript::ModifySpellDamageTaken(Unit* /*target*/, Unit* attacker, int32& damage, SpellInfo const* /*spellInfo*/)
+{
+    if (!attacker)
+    {
+        return;
+    }
+
+    if (attacker->IsPlayer())
+    {
+        return;
+    }
+
+    if (attacker->GetMapId() != TOS_MAP_ID)
+    {
+        return;
+    }
+
+    auto map = attacker->GetMap();
+    if (!map)
+    {
+        return;
+    }
+
+    auto instance = map->ToInstanceMap();
+    if (!instance)
+    {
+        return;
+    }
+
+    auto iScript = instance->GetInstanceScript();
+    if (!iScript)
+    {
+        return;
+    }
+
+    uint32 currentWave = iScript->GetData(TOS_DATA_ENCOUNTER_CURRENT_WAVE);
+
+    uint32 baseDamage = sConfigMgr->GetOption<uint32>("TrialOfStrength.Scaling.BaseDamage.Spell", 500);
+    uint32 damageDivider = sConfigMgr->GetOption<uint32>("TrialOfStrength.Scaling.BaseDamage.SpellDivider", 15);;
 
     uint32 newDamage = baseDamage * (1.0f + (float(currentWave) / float(damageDivider)));
 
--- src/scripts/ToSUnitScript.h
+++ src/scripts/ToSUnitScript.h
@@ -13,6 +13,7 @@ public:
     void OnUnitDeath(Unit* /*unit*/, Unit* /*killer*/) override;
 
     void ModifyMeleeDamage(Unit* /*target*/, Unit* /*attacker*/, uint32& /*damage*/) override;
+    void ModifySpellDamageTaken(Unit* /*target*/, Unit* /*attacker*/, int32& /*damage*/, SpellInfo const* /*spellInfo*/) override;
 };
 
 #endif // MODULE_TRIAL_OF_STRENGTH_UNIT_SCRIPT_H
